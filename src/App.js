22485d89634d1b9bc5b90bd198b5d88853b2f162
Robbe Van Looy <robbe.vanlooy@student.hogent.be>
2020-10-03 21:35:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to the recipe app!
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here a list of our recipes:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
