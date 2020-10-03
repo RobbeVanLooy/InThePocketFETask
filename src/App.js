73fb2db59d7b2297eb15ba5f6185c0f02d8d77d3
Robbe Van Looy <robbe.vanlooy@student.hogent.be>
2020-10-03 23:57:42 +0200

    added connection to cms + tested graphql query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import { useQuery } from "graphql-hooks";
+//import { Image } from "react-datocms";
+import "./App.css";
+
+const RECIPE_QUERY = `query Recipe {
+  allRecipes {
+    title
+    recipeimage {
+      alt
+      filename
+    }
+    description
+    externallink
+    recipeingredients {
+      item
+      amount
+      unit
+    }
+  }
+}`;
 
 function App() {
+  const { loading, error, data } = useQuery(RECIPE_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return "Something Bad Happened";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Welcome to the recipe app!
-        </h1>
-        <p>
-          Here a list of our recipes:
-        </p>
-        
-      </header>
+    <div>
+      {JSON.stringify(data)}
+      {data.allRecipes.map((recipe) => (
+        <article>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.recipeimage.filename}</p>
+          <p>{recipe.description}</p>
+          <p>{recipe.externallink}</p>
+          {recipe.recipeingredients.map((ingredient) => (
+            <p>
+              {ingredient.item} {ingredient.amount} {ingredient.unit}
+            </p>
+          ))}
+        </article>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+//import Recipe from './Recipe';
+import * as serviceWorker from "./serviceWorker";
+import { GraphQLClient, ClientContext } from "graphql-hooks";
+
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    Authorization: "Bearer c645d1f4fb55a99780e46147044449",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClientContext.Provider value={client}>
+      <App />
+    </ClientContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
